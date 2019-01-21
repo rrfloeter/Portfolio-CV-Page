48012702d97d3ce8d14a5258110b418b2508214f
rfloeter <rainer.r.floeter@gmail.com>
2019-01-20 21:44:55 -0500

    comment fixes

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,21 @@
 //change image when scrolling
 $(window).scroll(function () {
     var img_url = "assets/last_one.jpg";
+
+    //test
     console.log($(window).scrollTop());
 
     if ($(window).scrollTop() > 580) {
         img_url = "assets/red.jpg";
     }
     $('.body').css('background-image', 'url(' + img_url + ')');
-    // $('.body').css('background-size':'100% 100%');
 
+    //hide boxes until scroll down is complete
     if ($(window).scrollTop() >= 580) {
-        $(".bar1").show();
-        $("#box1").show();
-        $("#box2").show();
-        $("#box3").show();
+        $(".bar1").fadeIn();
+        $("#box1").fadeIn();
+        $("#box2").fadeIn();
+        $("#box3").fadeIn();
     } else {
         $(".bar1").hide();
         $("#box1").hide();
@@ -21,20 +23,22 @@ $(window).scroll(function () {
         $("#box3").hide();
     }
 
-   
+
 });
 
+//scroll happens when downbutton is hit
 $(function () {
-$("#downbutton").click(function() {
-    $("html, body").animate({ scrollTop: 744 }, "slow");
-  });
+    $("#downbutton").click(function () {
+        $("html, body").animate({ scrollTop: 744 }, "slow");
+    });
 
-  $(window).scroll(function(){
-    var scroll = $('#scroll');
-    if ($(this).scrollTop() > 400) {
-        scroll.fadeIn('slow');
-    } else {
-        scroll.fadeOut('slow');
-    }
-});
+    //combats the issue of the screen spazzing out when attempting to scroll back up or further down after hitting the arrow button
+    $(window).scroll(function () {
+        var scroll = $('#scroll');
+        if ($(this).scrollTop() > 400) {
+            scroll.fadeIn('slow');
+        } else {
+            scroll.fadeOut('slow');
+        }
+    });
 });
